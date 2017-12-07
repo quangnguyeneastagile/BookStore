8b6fb60e79474d00aeab04265b329e6cb49975ba
quangnguyeneastagile <quang.nguyen@eastagile.com>
2017-12-07 15:54:41 +0700

    Index page + filter

--- bookstore_library/bookstore/bookstore/catalog/urls.py
+++ bookstore_library/bookstore/bookstore/catalog/urls.py
@@ -5,6 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.index, name='index'),
-    url(r'^books/$', views.book_list_view, name = 'books'),
     url(r'^book/(?P<pk>\d+)$', views.book_detail_view, name='book-detail'),
 ]
--- bookstore_library/bookstore/bookstore/catalog/views.py
+++ bookstore_library/bookstore/bookstore/catalog/views.py
@@ -3,14 +3,35 @@ from django.http import Http404
 from django.views import generic
 from .models import *
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+import requests
+
 # Create your views here.
+
 def index(request):
     num_books = Book.objects.all().count()
     num_authors = Author.objects.all().count()
+
+    book_list = Book.objects.filter(Category__name__contains='Hugo CDE')
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(book_list,2) #2 items per page
+    try:
+        book_list = paginator.page(page)
+    except PageNotAnInteger:
+        book_list = paginator.page(1)
+    except EmptyPage:
+        book_list = paginator.page(paginator.num_pages)
+
     return render(request,
                 'index.html',
-                context={'num_books':num_books,'num_authors':num_authors,},
+                context={'num_books':num_books,'book_list':book_list,},
     )
+
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
 
 def book_detail_view(request,pk):
     try:
@@ -29,14 +50,9 @@ def book_list_view(request):
         raise Http404("No book in the store!")
     #book_id=get_object_or_404(Book, pk=pk)
     else:
-        category_objs = Category.objects.all()
         print("Catergory inside: ", category_objs)
         return render(
             request,
             'catalog/book_list.html',
             context={'category_objs':category_objs,}
         )
-
-class BookListView(generic.ListView):
-    template_name = 'ctalog/book_list.html'
-    context_object_name = 'book_list'
